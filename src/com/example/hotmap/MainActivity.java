e882c47c6a6fd9e1be19d762c2fe6e9f231c07d5
Jeffrey He <jhe12@terpmail.umd.edu>
2014-09-06 16:15:05 -0400

    SHA1 key updated and HeatMap template added. Also modified to be more fragment friendly.

--- src/com/example/hotmap/MainActivity.java
+++ src/com/example/hotmap/MainActivity.java
@@ -1,13 +1,91 @@
 package com.example.hotmap;
 
+import java.util.List;
+
+import com.google.android.gms.maps.MapFragment;
+
+import org.json.JSONException;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
+	 private GoogleMap googleMap;
+	
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    } 
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+     // latitude and longitude
+        double latitude = 42.2780440;
+        double longitude = -83.7382240;
+         
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(new LatLng(latitude, longitude)).title("Jeff");
+         
+        // adding marker
+        googleMap.addMarker(marker);
+    }
+    
+    
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
+
+    //This heatmap area needs JSON and needs work
+  /*  private void addHeatMap() {
+        List<LatLng> list = null;
+
+        // Get the data: latitude/longitude positions of police stations.
+        try {
+            list = readItems(R.raw.police_stations);
+        } catch (JSONException e) {
+            Toast.makeText(this, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+        }
+
+        // Create a heat map tile provider, passing it the latlngs of the police stations.
+         mProvider = new HeatmapTileProvider.Builder()
+            .data(list)
+            .build();
+        // Add a tile overlay to the map, using the heat map tile provider.
+        mOverlay = googleMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+    } */
+    
 }
